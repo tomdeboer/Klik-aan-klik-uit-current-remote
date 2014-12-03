079360cfb29d3c46d09fb70912e207ad873c18c6
Tom Boer <tom@tomdeboer.nl>
2014-12-03 23:55:24 +0100

    Added RCswitch

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -3,13 +3,17 @@
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 
+#include <RCSwitch.h>
+
 // Use pin 2 as wake up pin
 const int wakeUpPin = 4;
 const int dataPin   = 3;
 
+RCSwitch relay = RCSwitch();
+
 void wakeUp()
 {
-    digitalWrite(dataPin, digitalRead(wakeUpPin));
+    relay.switchOn(1, (int) digitalRead(wakeUpPin));
 }
 
 void setup()
@@ -17,8 +21,10 @@ void setup()
     // Configure wake up pin as input.
     // This will consumes few uA of current.
     pinMode(wakeUpPin, INPUT);   
-    pinMode(dataPin, OUTPUT);   
-      
+    pinMode(dataPin, OUTPUT);
+
+    relay.enableTransmit(dataPin);
+
     GIMSK |= (1<<PCIE); // Turn on Pin Change interrupt
     PCMSK |= (1<<PCINT4); // Which pins are affected by the interrupt
 
@@ -28,11 +34,11 @@ void loop()
 {
     // Enter power down state with ADC and BOD module disabled.
     // Wake up when wake up pin is low.
-   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-   wakeUp();
-//  digitalWrite(dataPin, digitalRead(wakeUpPin));
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    wakeUp();
+
 }
 
 ISR(PCINT0_vect) {
-//  wakeUp();
+    // Just waking up here
 }
